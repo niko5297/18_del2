5f9b0d804b09722031c6e6846046008692386974
nikolajwassmann <nwassmann@hotmail.com>
2018-11-06 17:43:34 +0100

    Taget Terning klasse fra sidste CDIO

--- 18_del2/src/Terning.java
+++ 18_del2/src/Terning.java
@@ -1,2 +1,8 @@
 public class Terning {
+    public int kast() {
+        float t1 = (float) Math.random();
+        float t2 = t1 * 5;
+        int t3 = Math.round(t2);
+        return t3 + 1;
+    }
 }
